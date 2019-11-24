163a99dd61213449ed309146904d091ed8b42265
Mehmet Umut İNCEER <mumutinceer@gmail.com>
2019-11-24 22:21:23 +0300

    Question 34
    https://www.w3resource.com/c-programming-exercises/for-loop/index.php

---
+++ cozum34.2.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// Question 34   for-loop
+
+int main()
+{
+	int start, end, i,counter;
+	
+	
+	puts("Input starting number of range:");
+	scanf("%d",&start);
+	puts("Input ending number of range:");
+	scanf("%d",&end);
+	printf("The prime number between %d and %d are ",start,end);
+
+		
+		for( i = start; i <=end; i++)
+		{
+			counter = 0;
+			
+			for(int k=2; k <= i/2; k++)
+			{
+				if(i % k == 0)
+				{
+					counter++;
+					break;
+				}	
+			}
+			
+			if(counter == 0 && i !=1 )
+			{
+				printf("%d ",i);
+			}
+		}
+		
+	
+	return 0;
+}
