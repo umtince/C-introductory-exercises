5e7a241a0b392cfd949c67aeab9b17443e2559f0
umtince <54290546+umtince@users.noreply.github.com>
2019-11-25 00:21:49 +0300

    Question 9

--- piramit.cpp
+++ piramit.cpp
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
+//Question 9 https://www.w3resource.com/c-programming-exercises/for-loop/index.php
+
 int main()
 {
    
   	int satir;
   	scanf("%d",&satir);
 		
-   for(int i=1; i<= satir; i++)  //satýr 
+   for(int i=1; i<= satir; i++)  //satÃ½r 
    {
    	for(int yildiz=1; yildiz<= i; yildiz++)
    	{
