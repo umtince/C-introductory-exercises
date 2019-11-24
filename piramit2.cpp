154f1a153ef242470ad550830c58b016730396fb
Mehmet Umut İNCEER <mumutinceer@gmail.com>
2019-11-24 21:15:21 +0300

    Question 10
    https://www.w3resource.com/c-programming-exercises/for-loop/index.php

---
+++ piramit2.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+	int satir;
+	scanf("%d",&satir);
+	int sayi = 1;
+	
+	for(int i=1; i <=satir; i++)
+	{
+		for(int j=1; j<=i; j++)
+		{
+			printf("%d",sayi);
+			sayi++;
+			
+		}
+		sayi = 1;
+		printf("\n");
+	}
+	return 0 ;
+}
