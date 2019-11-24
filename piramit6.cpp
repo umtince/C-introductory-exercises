913f94371b90b6cc4605672d690424430ba9aeed
Mehmet Umut İNCEER <mumutinceer@gmail.com>
2019-11-24 21:18:21 +0300

    Question 14
    https://www.w3resource.com/c-programming-exercises/for-loop/index.php

---
+++ piramit6.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	int satir;
+	scanf("%d",&satir);
+	int bosluk=satir-1;
+	for(int i=1; i<=satir; i++)
+	{
+		for(int a=bosluk; a>=0; a--)
+		{
+			printf(" ");
+		}
+		for(int j=1; j<=i; j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+		bosluk--;
+	}
+	
+	return 0;
+}
